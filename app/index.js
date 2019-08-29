61e0f9d845d20513ef15131510e3a3e2485cd413
QFernando <f.quintana88@yahoo.com.ar>
2019-08-29 12:44:56 -0300

    Change the query to use a wildcard
    Added an input for obtaining both an autocomplete response or a suggestion

--- app/index.js
+++ app/index.js
@@ -111,5 +111,54 @@ app.get('/location/suggest', async function (request, response) {
     response.send(body.suggest.simple_phrase[0].options);
 });
 
+/**
+ * Searches for locations matching the provided term
+ * or makes a suggestion for the autocomplete
+ * @param term string
+ * @returns json[]
+ */
+app.get('/location/search-and-suggest', async function (request, response) {
+    // search for suggestions for location name in
+    // the request term
+    const { body } = await client.search({
+        index: elasticSearchIndex,
+        // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
+        body: {
+            query: {
+                wildcard: {
+                    loc_name: {
+                        value: request.query.term + '*'
+                    }
+                }
+            },
+            suggest: {
+                text: request.query.term,
+                simple_phrase: {
+                    phrase: {
+                        field: "loc_name.trigram",
+                        size: 1,
+                        gram_size: 3,
+                        direct_generator: [{
+                            field: "loc_name.trigram",
+                            prefix_length: 0,
+                            suggest_mode: "missing"
+                        }],
+                        highlight: {
+                            pre_tag: "<em>",
+                            post_tag: "</em>"
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+    if (body.hits.hits.length > 0) {
+        response.send(body.hits.hits);
+    } else {
+        response.send(body.suggest.simple_phrase[0].options);
+    }    
+});
+
 // Listen to port: 3000
 app.listen(3000);
